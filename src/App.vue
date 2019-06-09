19ca60806c76aef75714bfbe64798c6622f0f235
weburco <47752219+weburco@users.noreply.github.com>
2019-06-09 17:57:54 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,7 @@
       </div>
     </div>
     <div class="wrap">
-      <div class="bg-output col-6 col-sm-10">
+      <div class="bg-output col-6 col-sm-12">
         <div class="d-flex" style="justify-content:space-between;">
           <h4>Output</h4>
           <div style="cursor:pointer" @click="copyOutput" ref="copyElement">Copy</div>
